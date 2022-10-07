ffc47d1afc31e977ca7898c02ce906818ea34bdd
Ellis Aston <ellisaston@outlook.com>
2022-10-07 02:08:28 +0100

    Added lines to dotfiles.sh

--- private_scripts/dotfiles.sh
+++ private_scripts/dotfiles.sh
@@ -2,7 +2,7 @@
 
 #Installing Basics
 echo "Installing Pacman Packages"
-sudo pacman -S zsh gddm i3-gaps kitty neofetch neovim polybar dunst rofi
+sudo pacman -S zsh starship gddm i3-gaps kitty neofetch neovim polybar dunst rofi
 echo "Installing AUR Packages"
 yay -S chezmoi zplug picom-ibhagwan-git autotiling plex-mpv-shim neovim-plug betterlockscreen 
 
@@ -11,4 +11,5 @@ chsh -s /bin/zsh
 
 #Cloning dotfiles with chezmoi
 chezmoi init --apply https://github.com/insomniac-afk/dotfiles.git
+chmod +x $HOME/.config/i3/autostart.sh
 
