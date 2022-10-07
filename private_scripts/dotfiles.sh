c920992dc38e47fde062976eda541fa526c2f0d1
Ellis Aston <ellisaston@outlook.com>
2022-10-07 02:26:00 +0100

    Added lines to dotfiles.sh

--- private_scripts/dotfiles.sh
+++ private_scripts/dotfiles.sh
@@ -2,7 +2,7 @@
 
 #Installing Basics
 echo "Installing Pacman Packages"
-sudo pacman -S zsh starship gddm i3-gaps kitty neofetch neovim polybar dunst rofi
+sudo pacman -S zsh starship i3-gaps kitty neofetch neovim polybar dunst rofi
 echo "Installing AUR Packages"
 yay -S chezmoi zplug picom-ibhagwan-git autotiling plex-mpv-shim neovim-plug betterlockscreen 
 
