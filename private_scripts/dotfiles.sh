06ebbcd3f684e6b7492478c17f5436e16073c0f6
Ellis Aston <ellisaston@outlook.com>
2022-10-07 01:30:46 +0100

    Corrected spelling in dotfiles.sh

--- private_scripts/dotfiles.sh
+++ private_scripts/dotfiles.sh
@@ -4,7 +4,7 @@
 echo "Installing Pacman Packages"
 sudo pacman -S zsh gddm i3-gaps kitty neofetch neovim polybar dunst rofi
 echo "Installing AUR Packages"
-sudo yay -S chezmoi zplug picom-ibhagwan-git autotiling-git plex-mpv-shim neovim-plug betterlockscreen 
+yay -S chezmoi zplug picom-ibhagwan-git autotiling-git plex-mpv-shim neovim-plug betterlockscreen 
 
 #Cloning dotfiles with chezmoi
 chezmoi init --apply https://github.com/insomniac-afk/dotfiles.git
